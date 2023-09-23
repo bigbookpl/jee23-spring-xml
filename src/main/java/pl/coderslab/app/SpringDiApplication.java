05ed06db720c53ab2bda3a1196c32b369119cef1
Krzysztof Konieczny <bigbookpl@users.noreply.github.com>
2023-09-23 11:10:53 +0200

    Zadanie 1 i 2

--- src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,11 +1,16 @@
 package pl.coderslab.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.HelloWorld;
 
 public class SpringDiApplication {
 
     public static void main(String[] args) {
       ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+      HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
+      helloWorld.printMessage();
+
       context.close();
     }
 
